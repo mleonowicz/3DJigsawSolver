434cd7c0e33d34887c3aae3bd32ca9900ab5cb1f
Jacek Kowalski <jkowalski@antmicro.com>
2023-01-17 22:21:50 +0100

    Added number of pieces as user arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 
 from JigsawSolver.core import Puzzle
+from JigsawSolver.genetic_algorithm import GeneticAlgorithm
 from JigsawSolver.video_utility import parse_video, save_puzzle_video
 
 
@@ -13,14 +14,35 @@ def main():
         type=str,
         default='example/example.mp4',
     )
+    parser.add_argument(
+        '--pieces-number-x',
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        '--pieces-number-y',
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        '--pieces-number-z',
+        type=int,
+        default=10
+    )
 
     args = parser.parse_args()
-    index_mapping, metadata = parse_video(args.video_path, 128, 72, 5)  # noqa: F841
+    index_mapping, metadata = parse_video(
+        args.video_path,
+        args.pieces_number_x,
+        args.pieces_number_y,
+        args.pieces_number_z
+    )  # noqa: F841
+    ga = GeneticAlgorithm(index_mapping, 100, 20)
+    _, p = ga.fit(100)
     save_puzzle_video(
         "build/result.avi",
-        Puzzle(index_mapping),
-        metadata,
-        128, 72, 5
+        p,
+        metadata
     )
 
 
